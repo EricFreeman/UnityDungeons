d7cd1b3922c2a34e28d0dabb622a0eea6bcbda45
Eric Freeman <eric.freeman@extendhealth.com>
2014-12-11 21:23:04 -0700

    Simple dungeon generation using Adam's dungeon project.

---
+++ Assets/Resources/Scripts/Director.cs
@@ -0,0 +1,26 @@
+﻿using Dungeon.Generator.Generation;
+using UnityEngine;
+
+namespace Assets.Resources.Scripts
+{
+    public class Director : MonoBehaviour
+    {
+        public GameObject Tile;
+
+        void Start()
+        {
+            var g = Generator.Generate(MapSize.Small);
+            for (var z = 0; z < g.Height; z++)
+            {
+                for (var x = 0; x < g.Width; x++)
+                {
+                    if (g[x, z] == 1)
+                    {
+                        var t = (GameObject) Instantiate(Tile);
+                        t.transform.position = new Vector3(x, 0, z);
+                    }
+                }
+            }
+        }
+    }
+}
