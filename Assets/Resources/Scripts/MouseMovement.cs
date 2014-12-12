72cc8cd2e5f9e4ec5de66291a2b5981f19b34194
Eric Freeman <eric.freeman@extendhealth.com>
2014-12-12 09:21:17 -0700

    Also walk with keyboard now thus making the scripts name dumb.

--- Assets/Resources/Scripts/MouseMovement.cs
+++ Assets/Resources/Scripts/MouseMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assets.Resources.Scripts
 {
@@ -8,6 +9,14 @@ namespace Assets.Resources.Scripts
         public float Speed = 3;
 
         private bool _isPositionSet;
+
+        public Dictionary<KeyCode, Vector3> KeyOffset = new Dictionary<KeyCode, Vector3>
+        {
+            {KeyCode.W, new Vector3(0, 0, 1)},
+            {KeyCode.S, new Vector3(0, 0, -1)},
+            {KeyCode.A, new Vector3(-1, 0, 0)},
+            {KeyCode.D, new Vector3(1, 0, 0)},
+        }; 
 
         void Update()
         {
@@ -22,13 +31,20 @@ namespace Assets.Resources.Scripts
         {
             if (Input.GetMouseButtonDown(0))
             {
-                var pos = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(pos, out hit))
-                {
-                    _isPositionSet = true;
-                    _movePosition = hit.collider.transform.position;
-                }
+                CheckForTile(Camera.main.ScreenPointToRay(Input.mousePosition));
+            }
+            if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+                CheckForTile(new Ray(transform.position - new Vector3(Input.GetAxisRaw("Horizontal"), -.1f, Input.GetAxisRaw("Vertical")), Vector3.down));
+            
+        }
+
+        private void CheckForTile(Ray pos)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(pos, out hit))
+            {
+                _isPositionSet = true;
+                _movePosition = hit.collider.transform.position;
             }
         }
     }
